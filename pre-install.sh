42a4a20b5eada63c0515881a07e6ee913c4f3677
Nils Verheyen <nils.verheyen@uni-due.de>
2022-02-10 06:24:01 +0100

    Update oh-my-zsh if already installed

--- pre-install.sh
+++ pre-install.sh
@@ -5,5 +5,8 @@ OMZDIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OMZDIR" ]; then
     echo "Installing oh-my-zsh"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "Updating oh-my-zsh"
+    cd $OMZDIR && git pull
 fi
 
