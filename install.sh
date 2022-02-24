376e48dfa705c9547c06261ac741045f05db7b23
Nils <nverheyen@googlemail.com>
2022-02-24 21:57:46 +0100

    Update home setup

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ case $1 in
    work|home)
      echo "Setting up dotfiles for $1"
      sudo pacman -S --needed ansible python
-     ansible-playbook -i localhost, -c local --ask-become-pass setup/$1.yml ;;
+     
+     # this needs to be done beforehand, so the yay module is available
+     ansible-playbook --ask-become-pass playbooks/yay.yml ;;
+
+     ansible-playbook --ask-become-pass setup/$1.yml ;;
    *)
      echo "Enter $0 work|home" ;;
 esac
