1dfa821a6e5252ede70b02490d13c8b04fb1b1ac
Nils <nverheyen@googlemail.com>
2022-02-28 21:46:14 +0100

    Remove yay from playbooks
    yay is installed during the installation step and should not be run in
    the setup playbooks

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 case $1 in
-   work|home)
-     echo "Setting up dotfiles for $1"
-     sudo pacman -S --needed ansible python
-     
-     # this needs to be done beforehand, so the yay module is available
-     ansible-playbook --ask-become-pass playbooks/yay.yml
+  work|home)
+    echo "Setting up dotfiles for $1"
+    sudo pacman -S --needed ansible python
 
-     ansible-playbook --ask-become-pass setup/$1.yml ;;
-   *)
-     echo "Enter $0 work|home" ;;
+    # this needs to be done beforehand, so the yay module is available
+    echo "Installing yay and necessary plugins"
+    ansible-playbook --ask-become-pass playbooks/yay.yml
+
+    echo "Executing setup"
+    ansible-playbook --ask-become-pass setup/$1.yml ;;
+  *)
+    echo "Enter $0 work|home" ;;
 esac
