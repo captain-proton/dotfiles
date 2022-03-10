42188f60da01abd4727ffc97e010e264dce01cba
Nils <nverheyen@googlemail.com>
2022-03-10 18:10:13 +0100

    Install yay only when needed

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 case $1 in
   work|home)
@@ -6,8 +6,12 @@ case $1 in
     sudo pacman -S --needed ansible python
 
     # this needs to be done beforehand, so the yay module is available
-    echo "Installing yay and necessary plugins"
-    ansible-playbook --ask-become-pass playbooks/yay.yml
+    yay=`pacman -Q yay`
+    if [[ $yay != yay* ]] && [[ ! -f playbooks/library/yay ]]
+    then
+      echo "Installing yay and necessary plugins"
+      ansible-playbook --ask-become-pass playbooks/yay.yml
+    fi
 
     echo "Executing setup"
     ansible-playbook --ask-become-pass setup/$1.yml ;;
