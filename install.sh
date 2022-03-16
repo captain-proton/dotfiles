ce83c13eec06821c1272664a859bb02dbdb97cfe
Nils <nverheyen@googlemail.com>
2022-03-16 16:11:07 +0100

    Run ansible and python installation only when necessary

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 case $1 in
   work|home)
     echo "Setting up dotfiles for $1"
-    sudo pacman -S --needed ansible python
+    ansible=`pacman -Q ansible`
+    python=`pacman -Q python`
+    if [[ $ansible != ansible* ]] || [[ $python != python* ]]
+    then
+      sudo pacman -S --needed ansible python
+    fi
 
     # this needs to be done beforehand, so the yay module is available
     yay=`pacman -Q yay`
