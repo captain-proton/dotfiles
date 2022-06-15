fb2d65b390fc421258d89495ce9df67ed38d9da3
Nils Verheyen <nils.verheyen@uni-due.de>
2022-06-15 13:36:18 +0200

    Update org mode settings

--- emacs/init.el
+++ emacs/init.el
@@ -152,7 +152,8 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +pretty +journal +pandoc
+            +hugo +dragndrop)                ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
