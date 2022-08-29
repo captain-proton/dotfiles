a403912e1d4ec3841ad2cba3c102bbbe30af21e0
Nils Verheyen <nils.verheyen@uni-due.de>
2022-08-29 11:45:43 +0200

    Add additional key bindings for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,6 +18,9 @@ return {
   -- If right padding is set to 0 then it will be increased
   -- to a single cell width
   enable_scroll_bar = true,
+  -- The color of the scrollbar "thumb"; the portion that represents the current viewport
+  scrollbar_thumb = '#444444',
+
 
   -- timeout_milliseconds defaults to 1000 and can be omitted
   leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 },
@@ -40,4 +43,34 @@ return {
       },
     },
   },
+  key_tables = {
+    copy_mode = {
+      { key="Escape", mods="NONE", action=act.CopyMode "Close" },
+      { key="h", mods="NONE", action=act.CopyMode "MoveLeft" },
+      { key="j", mods="NONE", action=act.CopyMode "MoveDown" },
+      { key="k", mods="NONE", action=act.CopyMode "MoveUp" },
+      { key="l", mods="NONE", action=act.CopyMode "MoveRight" },
+      -- { key=" ", mods="NONE", action=act.CopyMode "ToggleSelectionByCell" },
+      -- Enter search mode to edit the pattern.
+      -- When the search pattern is an empty string the existing pattern is preserved
+      { key="/", mods="SHIFT", action=act.Search { CaseSensitiveString = "" } },
+      -- navigate any search mode results
+      { key="n", mods="NONE", action=act.CopyMode "NextMatch" },
+      { key="N", mods="SHIFT", action=act.CopyMode "PriorMatch" },
+      -- Copy/yank to primary selection and leave copy mode
+      { key="y", mods="NONE", action=act.Multiple
+        {
+          act.CopyTo "PrimarySelection",
+          act.CopyMode "Close",
+        }
+      }
+    },
+    search_mode = {
+      { key="Escape", mods="NONE", action=act.CopyMode "Close" },
+      -- Go back to copy mode when pressing enter, so that we can use unmodified keys like "n"
+      -- to navigate search results without conflicting with typing into the search area.
+      { key="Enter", mods="NONE", action=act.ActivateCopyMode },
+      { key = 'u', mods = 'CTRL', action = act.CopyMode 'ClearPattern' },
+    },
+  }
 }
