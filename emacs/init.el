6be753c2822adb98279027c2b65e88d0f3815fc4
Nils <nils@ungerichtet.de>
2022-11-17 14:30:34 +0100

    Remove fuzzy find from ivy
    describe variable crashes when fuzzy find
    is turned on

--- emacs/init.el
+++ emacs/init.el
@@ -24,7 +24,7 @@
        (company +childframe)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +fuzzy +fonts +childframe)               ; a search engine for love and life
+       (ivy +fonts +childframe)               ; a search engine for love and life
        ;;vertico           ; the search engine of the future
 
        :ui
