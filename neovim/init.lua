e6b7e1421473c6998e437eb39202eedd24e2e0b9
Nils <nils@ungerichtet.de>
2023-04-04 07:55:43 +0200

    Fix neovim config

--- neovim/init.lua
+++ neovim/init.lua
@@ -234,7 +234,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'yaml', 'help' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'yaml', 'vimdoc' },
 
   highlight = { enable = true },
   indent = { enable = true },
