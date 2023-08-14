0d5c6bbbf40f350c52773054f3cb53941d499b2b
Nils <nils@ungerichtet.de>
2023-08-14 09:37:12 +0200

    Add direnv to neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -201,9 +201,13 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.autoformat',
+  require 'kickstart.plugins.debug',
 
+  {
+    'direnv/direnv'
+  },
+  
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
   --    up-to-date with whatever is in the kickstart repo.
