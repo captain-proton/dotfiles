aefbfed219fe0b45a63200ba3ff0e9b382b26d13
Nils <nils@ungerichtet.de>
2023-10-18 08:21:20 +0200

    Add support for spacemacs

---
+++ spacemacs/init.el
@@ -0,0 +1,25 @@
+;;; init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Nils Verheyen
+;;
+;; Author: Nils Verheyen <nils@ungerichtet.de>
+;; Maintainer: Nils Verheyen <nils@ungerichtet.de>
+;; Created: September 25, 2023
+;; Modified: September 25, 2023
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/captain-proton/init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'init)
+;;; init.el ends here
