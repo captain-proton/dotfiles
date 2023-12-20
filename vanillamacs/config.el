46f0d7a8cc03bab09ad3b756844496bc235a74de
Nils <nils@ungerichtet.de>
2023-12-20 10:21:30 +0100

    Update vanillamacs configuration

--- vanillamacs/config.el
+++ vanillamacs/config.el
@@ -1,3 +1,7 @@
+(defvar local-settings-file (format "%slocal.el" user-emacs-directory))
+(when (file-exists-p local-settings-file)
+  (load local-settings-file))
+
   (defvar elpaca-installer-version 0.6)
   (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
   (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
@@ -63,6 +67,13 @@
 
 (use-package evil-tutor)
 
+;; Using RETURN to follow links in Org/Evil 
+;; Unmap keys in 'evil-maps if not done, (setq org-return-follows-link t) will not work
+(with-eval-after-load 'evil-maps
+  (define-key evil-motion-state-map (kbd "SPC") nil)
+  (define-key evil-motion-state-map (kbd "RET") nil)
+  (define-key evil-motion-state-map (kbd "TAB") nil))
+
 (use-package evil-nerd-commenter
     :after evil
     :config
@@ -84,12 +95,10 @@
     :prefix "SPC"
     :global-prefix "M-SPC") ;; access leader key in insert mode
 
-
   (proton/leader-keys
    "." '(find-file :wk "Find file")
    "f c" '(lambda () (interactive) (find-file (concat user-emacs-directory "config.org")) :wk "Edit config.org")
    )
-
 
   (proton/leader-keys
    "b" '(:ignore t :wk "Buffer") ;; just a prefix, no real key binding
@@ -103,7 +112,6 @@
    )
 
   (proton/leader-keys
-
    "h" '(:ignore t :wk "Help") ;; just a prefix, no real key binding
    "h f" '(describe-function :wk "Describe function")
    "h k" '(describe-key :wk "Describe key")
@@ -124,6 +132,28 @@
    )
 
   )
+
+(use-package emacs
+  :elpaca nil
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
 
 (defvar proton/fixed-width-font "JetBrainsMono NF"
   "The font to use for monospaced (fixed width) text.")
@@ -154,18 +184,35 @@
 ;; Uncomment the following line if line spacing needs adjusting.
 (setq-default line-spacing 0.12)
 
+(defun proton/text-scale-reset ()
+  (interactive)
+  (text-scale-adjust 0))
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-=") 'proton/text-scale-reset)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 (add-to-list 'custom-theme-load-path (expand-file-name (concat user-emacs-directory "themes/")))
-(use-package nord-theme
-  :init (load-theme 'nord t))
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled, t by default
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled, t by default
+  ;; This is the default theme
+  (load-theme 'doom-nord t)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+  :config
+  (setq doom-modeline-height 24      ;; sets modeline height
+        doom-modeline-bar-width 5    ;; sets right bar width
+        doom-modeline-persp-name t   ;; adds perspective name to modeline
+        doom-modeline-persp-icon t)) ;; adds folder icon next to persp name
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode 1)
@@ -180,8 +227,73 @@
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package rainbow-mode
+  :diminish
   :hook
   ((org-mode prog-mode) . rainbow-mode))
+
+(use-package projectile
+  :config
+  (projectile-mode 1)
+  (proton/leader-keys
+    "p" '(:ignore t :wk "Project")
+    "p d" '(projectile-discover-projects-in-search-path :wk "Discover projects")
+    "p e" '(projectile-edit-dir-locals :wk "Edit project .dir-locals.el")
+    "p i" '(projectile-invalidate-cache :wk "Invalidate project cache")
+    "p p" '(project-switch-project :wk "Switch project")
+    "SPC" '(projectile-find-file :wk "Find file in project")
+  )
+)
+
+(use-package vertico
+  :ensure t
+  :bind (:map vertico-map
+         ("C-j" . vertico-next)
+         ("C-k" . vertico-previous)
+         ("C-f" . vertico-exit)
+         :map minibuffer-local-map
+         ("M-h" . backward-kill-word))
+  :custom
+  (vertico-cycle t)
+  :init
+  (vertico-mode))
+
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package savehist
+  :elpaca nil  ;; built-in to emacs, no package manager required
+  :init
+  (savehist-mode))
+
+(use-package consult
+  :config
+  (proton/leader-keys
+    "<" '(consult-buffer :wk "consult buffer")
+    "RET" '(consult-bookmark :wk "consult bookmark")
+    "f r" '(consult-recent-file :wk "consult recent file")
+    "m h" '(consult-org-heading :wk "consult org heading")
+    "s" '(:ignore t :wk "Search")
+    "s r" '(consult-ripgrep :wk "consult rg")
+    "s g" '(consult-grep :wk "consult grep")
+    "s G" '(consult-git-grep :wk "consult git grep")
+    "s f" '(consult-find :wk "consult find")
+    "s b" '(consult-line :wk "consult line")
+    "S y" '(consult-yank-from-kill-ring :wk "consult yank from kill ring")
+    "i" '(consult-imenu :wk "consult imenu"))
+  )
+
+(use-package marginalia
+  :after vertico
+  :ensure t
+  :custom
+  (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+  :init
+  (marginalia-mode))
+
+(setq org-return-follows-link t)
 
 (use-package toc-org
   :commands toc-org-enable
@@ -194,6 +306,14 @@
 (add-hook 'org-mode-hook 'org-indent-mode)
 (use-package org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(custom-set-faces
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.1))))
+ )
 
 (setq org-src-preserve-indentation t)
 
@@ -229,7 +349,7 @@
   :config
   
   (proton/leader-keys
-   "t" '(:ignore :wk "Templates")
+   "t" '(:ignore t :wk "Templates")
    "t c" '(tempel-complete :wk "Complete")
    "t i" '(tempel-complete :wk "Insert")
    )
@@ -239,6 +359,15 @@
 ;; The package is young and doesn't have comprehensive coverage.
 (use-package tempel-collection
   :after tempel)
+
+(use-package sudo-edit
+  :config
+  (proton/leader-keys
+    "f" '(:ignore t :wk "Files")
+    "f u" '(sudo-edit-find-file :wk "Sudo find file")
+    "f U" '(sudo-edit :wk "Sudo edit file")
+  )
+)
 
 (use-package which-key
   :init
