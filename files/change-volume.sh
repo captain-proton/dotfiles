d5a5edc19d9ff681d5e435acc9e80d9a674b2ac7
Nils <nils@ungerichtet.de>
2025-06-21 10:03:14 +0200

    Fix change-volume script
    How does wireplumber output muted
    state in different versions?

--- files/change-volume.sh
+++ files/change-volume.sh
@@ -16,7 +16,7 @@ esac
 # Query wpctl for the current volume and whether or not the speaker is muted
 volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2 * 100)}')
 mute=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $3}')
-if [ "$volume" -eq 0 ] || [ "$mute" = "yes" ]; then
+if [ "$volume" -eq 0 ] || [ "$mute" = "[MUTED]" ]; then
     # Show the sound muted notification
     dunstify -a "change-volume" -u low -i audio-volume-muted \
         -h string:x-dunst-stack-tag:$msgTag "Volume muted"
